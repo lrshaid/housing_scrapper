8feb650ed9bf8ff68786138d1b5d12becbcb4f5a
lrshaid <lautarorshaid@outlook.com>
2024-06-23 17:11:05 -0300

    first version"working"

--- main_test.py
+++ main_test.py
@@ -1,17 +1,23 @@
 from providers.mercadolibre import Mercadolibre
 from providers.baseprovider import BaseProvider
+from providers.processor import process_properties
+import yaml
+#provider_name = 'mercadolibre'
+#provider_data = {'base_url':'https://inmuebles.mercadolibre.com.ar','sources':['/departamentos/departamento-3-ambientes']}
 
-provider_name = 'mercadolibre'
-provider_data = {'base_url':'https://inmuebles.mercadolibre.com.ar','sources':['/departamentos/departamento-3-ambientes']}
+with open("configuration.yml", 'r') as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
 
-provider=Mercadolibre(provider_name, provider_data)#.props_in_source(provider_data)
+for provider_name, provider_data in cfg['providers'].items():
+    
+    #provider=Mercadolibre(provider_name, provider_data)#.props_in_source(provider_data)
+    process_properties(provider_name,provider_data)
 
-properties =[]
-print(provider)
 
-for prop in provider.next_prop():
- 
-    properties.append(prop)
 
-properties
+#for prop in provider.next_prop():
+# 
+#    properties.append(prop)
+#
+#print(properties)
 #"Mercadolibre","https://inmuebles.mercadolibre.com.ar/departamentos/departamento-3-ambientes"
---
+++ query_runner.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('properties.db')
+
+# Check to see if we know it
+stmt = 'DELETE FROM properties'#WHERE internal_id=:internal_id AND provider=:provider
+with conn:
+    #for prop in provider.next_prop():
+    cur = conn.cursor()
+    #logging.info(f"Processing property {prop['internal_id']}")
+    cur.execute(stmt)
+    result = cur.fetchone()
+    cur.close()
+    print(result)
