744c3530528f778a1a81c09fa1ea49b58aee9187
lrshaid <lautarorshaid@outlook.com>
2024-07-16 23:58:00 -0300

    fixing mercadolibre struct

--- providers/mercadolibre.py
+++ providers/mercadolibre.py
@@ -53,19 +53,26 @@ class Mercadolibre(BaseProvider):
                     price_section = info.find('div', class_='ui-search-item__group__element ui-search-item__group--price-grid').getText().strip().replace(".","")
 
                     title_section = section.getText()
-                    title = section.text#.find('span').get_text().strip() + \   ': ' + title_section.find('h2').get_text().strip()
+                    title = section.text
                     if price_section is not None:
                         title = title + ' ' + price_section.strip()
 
-                elif prop.find('div',class_='poly-card__content') is not None:
+                elif prop.find('div',class_="poly-card__content") is not None:
 
-                    info=prop.find('div',class_= 'poly-card__content')
+                    info=prop.find('div',class_= "poly-card__content")
                     section = info.find('a', class_ ="poly-component__title")
-                    href = section.next.attrs['href']
+                    href = section.attrs['href']
                     matches = re.search(regex, href)
                     internal_id = matches.group(1).replace('-', '')
-                    price_section = info.find('div', class_='poly-component__price').getText().strip().replace(".","")
-        
+                    price_section = info.find('div', class_="poly-component__price").getText().strip().replace(".","")
+                    title_section = section.getText()
+                    title = section.text
+
+                elif prop.find('div',class_="poly-card__content") is None:
+
+                    info=prop.find('div',class_= "poly-card__content")
+                    info
+
                 yield {
                     'title': title_section, 
                     'url': href,
