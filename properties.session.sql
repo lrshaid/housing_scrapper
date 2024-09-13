f324a94a3b79a9c68d1c0b9dfd15a5cac47775c9
lrshaid <lautarorshaid@outlook.com>
2024-09-12 23:40:03 -0300

    updates

--- properties.session.sql
+++ properties.session.sql
@@ -1,7 +1,7 @@
 
--- DELETE FROM properties WHERE captured_date >= '2024-07-29'
+DELETE FROM properties WHERE captured_date >= '2024-08-12'
 
-SELECT * FROM properties -- WHERE captured_date >= '2024-07-29'
+--SELECT * FROM properties -- WHERE captured_date >= '2024-07-29'
 
 
 -- SELECT * FROM properties WHERE captured_date < '2024-07-28'
